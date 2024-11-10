26e95c65b48e518f2e39f2c5d750b9d064bbe178
yehiasalah <yehiasalah74@gmail.com>
2024-11-10 19:03:35 +0200

    ASdasd

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,11 +1,12 @@
 # relationship_app/urls.py
 from django.urls import path
 from .views import login_view, logout_view, register_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register_view, name='register'),  # Custom registration view
 ]
