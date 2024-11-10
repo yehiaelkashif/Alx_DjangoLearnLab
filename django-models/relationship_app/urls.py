2b2d69a08ab976e8832cc939867ff849f62c0889
yehiasalah <yehiasalah74@gmail.com>
2024-11-10 19:20:26 +0200

    Asdasd

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/add_book/', views.add_book, name='"add_book'),
+    path('books/edit_book/<int:pk>/', views.edit_book, name='edit_book'),
     path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
 ]
