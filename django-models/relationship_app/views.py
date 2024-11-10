f73990efcba7685cc735d3cfd3b988c973b578f6
yehiasalah <yehiasalah74@gmail.com>
2024-11-10 18:20:29 +0200

    asdads

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Library
 from .models import Book # Explicitly importing Library and Book models
-
+from django.views.generic.detail import DetailView
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
