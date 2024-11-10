813eaca15144e1ee1d011b94ee448be64170c300
yehiasalah <yehiasalah74@gmail.com>
2024-11-10 18:37:34 +0200

    asdasd

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,11 +1,10 @@
 # relationship_app/urls.py
 from django.urls import path
-from .views import admin_view, librarian_view, member_view
+from .views import list_books, LibraryDetailView
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    path('admin-view/', admin_view, name='admin_view'),
-    path('librarian-view/', librarian_view, name='librarian_view'),
-    path('member-view/', member_view, name='member_view'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
