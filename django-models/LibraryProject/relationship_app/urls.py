7ee11affd5561a327e42c50f6f159deee4afec81
yehiasalah <yehiasalah74@gmail.com>
2024-11-11 22:32:26 +0200

    AsdasD

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 # relationship_app/urls.py
 from django.urls import path
 from . import views
+from .views import list_books
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # Function-based view
