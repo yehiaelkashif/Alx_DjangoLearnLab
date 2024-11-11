43ba69f8b5cdd672e0ae40888c5cdeb4935d0a8a
yehiasalah <yehiasalah74@gmail.com>
2024-11-11 22:20:50 +0200

    asdsa

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,8 @@
+# relationship_app/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('books/', views.list_books, name='list_books'),  # Function-based view
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,34 +1,17 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm
+# relationship_app/views.py
+from django.shortcuts import render
+from .models import Book  # Assuming you have a Book model
+from django.views.generic import DetailView
+from .models import Library
 
-# User Registration view
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.save()
-            login(request, user)
-            return redirect('list_books')  # Redirect to a page after successful registration
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
 
-# User Login view
-def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('list_books')  # Redirect to a page after successful login
-    else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
 
-# User Logout view
-def user_logout(request):
-    logout(request)
-    return render(request, 'logout.html')
+def list_books(request):
+    books = Book.objects.all()  # Query all books from the database
+    return render(request, 'list_books.html', {'books': books})
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'  # Specify the template to use
+    context_object_name = 'library'  # This is the name to use in the template for the object
