bd8dea0c00c39451f11ffa5378c521dcde77e74d
yehiasalah <yehiasalah74@gmail.com>
2024-11-11 22:04:28 +0200

    SAdsad

--- Introduction_to_Django/LibraryProject/relationship_app/models.py
+++ Introduction_to_Django/LibraryProject/relationship_app/models.py
@@ -1,18 +1,32 @@
-# relationship_app/models.py
 from django.db import models
-from django.contrib.auth.models import User
 
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    publication_year = models.IntegerField()
-
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
+# Author model with a one-to-many relationship to Book
+class Author(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return self.name
+
+# Book model with a ForeignKey to Author
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+# Library model with a many-to-many relationship to Book
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
+
+# Librarian model with a one-to-one relationship to Library
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return self.name
