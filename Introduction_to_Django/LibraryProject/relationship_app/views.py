2eb1a13220afc02d384f3bf1a813fdb09dfabd69
yehiasalah <yehiasalah74@gmail.com>
2024-11-16 13:19:01 +0200

    ASd

--- Introduction_to_Django/LibraryProject/relationship_app/views.py
+++ Introduction_to_Django/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     if request.method == 'POST':
