553b2092fd1fcd05ad755967532431499122317f
yehiasalah <yehiasalah74@gmail.com>
2024-11-16 13:53:40 +0200

    sad

--- Introduction_to_Django/LibraryProject/relationship_app/models.py
+++ Introduction_to_Django/LibraryProject/relationship_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+["class UserProfile(models.Model):", "Admin", "Member"]
+
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
