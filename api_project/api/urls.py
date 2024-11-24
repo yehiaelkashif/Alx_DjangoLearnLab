7ca4ec7e2b272e3c6a99bc4acd30584b08efa195
yehiasalah <yehiasalah74@gmail.com>
2024-11-24 21:22:35 +0200

    asdsad

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
-from .views import BookList
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, BookList
+from django.urls import path, include
+
+# Initialize the router
+router = DefaultRouter()
+
+# Register the BookViewSet with the router
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Route for the BookList view (if still needed for listing)
     path('books/', BookList.as_view(), name='book-list'),
+
+    # Include the router-generated URLs
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,35 +1,10 @@
-
-#Using a simple view
-
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import generics
-from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
-#Setting up a view that uses the serializer to retrieve and return book data.
-class BookListAPIView(generics.ListAPIView):
-    #implementing authentication
-    authentication_classes = [TokenAuthentication]
-    #ensuring only authenticated users can access the data from this view
-    permission_classes = [IsAuthenticated]   
+class BookViewSet(ModelViewSet):
+    """
+    A ViewSet for performing CRUD operations on the Book model.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-# Implementing CRUD Operations with ViewSets and Routers 
-# in Django REST Framework using ViewSets
-# Create your views here.
-
-class BookViewSet(viewsets.ModelViewSet):
-    #authenticating users before they can perform CRUD operations
-    authentication_classes = [TokenAuthentication]
-   
-   #ensuring only authenticated users can access the data from this view
-    permission_classes = [IsAuthenticated, IsAdminUser]
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
- 
-
