9b6e3c2ef08ebabca68a9273af641e25b0a2e629
yehiasalah <yehiasalah74@gmail.com>
2024-11-24 18:56:44 +0200

    asdsad

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
+# Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.title
+    title = models.CharField(max_length = 50)
+    author = models.CharField(max_length = 50)
+    
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,35 @@
+
+#Using a simple view
+
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-class BookList(ListAPIView):
-    queryset = Book.objects.all()  # Retrieve all Book instances
-    serializer_class = BookSerializer  # Use BookSerializer for serialization
+# Create your views here.
+#Setting up a view that uses the serializer to retrieve and return book data.
+class BookListAPIView(generics.ListAPIView):
+    #implementing authentication
+    authentication_classes = [TokenAuthentication]
+    #ensuring only authenticated users can access the data from this view
+    permission_classes = [IsAuthenticated]   
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Implementing CRUD Operations with ViewSets and Routers 
+# in Django REST Framework using ViewSets
+# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    #authenticating users before they can perform CRUD operations
+    authentication_classes = [TokenAuthentication]
+   
+   #ensuring only authenticated users can access the data from this view
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+ 
+
